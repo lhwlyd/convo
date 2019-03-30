dff3454ea0f9b18e7778af426989502923981fba
lhwlyd <yil623@ucsd.edu>
2019-03-30 14:24:04 -0700

    redux and login page setup

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div style={{ height: "75vh" }} className="container valign-wrapper">
+        <div className="row">
+          <div className="col s12 center-align">
+            <h4>
+              <b>Chat</b> in <span style={{ fontFamily: "monospace" }}>3D</span>
+            </h4>
+            <p className="flow-text grey-text text-darken-1">
+              A place where you belong
+            </p>
+            <br />
+
+            <Link
+              to="/register"
+              style={{
+                width: "150px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px"
+              }}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            >
+              Register
+            </Link>
+
+            <Link
+              to="/login"
+              style={{
+                marginLeft: "2rem",
+                width: "150px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px"
+              }}
+              className="btn btn-large waves-effect white hoverable black-text"
+            >
+              Log In
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Landing;
